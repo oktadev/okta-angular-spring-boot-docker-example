9458b76e98cda130763cfd10ab2bfa050db78869
Matt Raible <matt.raible@okta.com>
2020-02-24 15:55:38 -0700

    Fix tests

--- notes/e2e/src/app.e2e-spec.ts
+++ notes/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('notes app is running!');
+    expect(page.getTitleText()).toEqual('Notes');
   });
 
   afterEach(async () => {
